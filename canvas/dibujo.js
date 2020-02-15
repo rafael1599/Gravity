34335a4b8094ebce39520ccd7510a80a27a65e3a
rafo1599 <rafaelukf@gmail.com>
2020-02-14 22:48:30 -0500

    funciones en JavaScript

---
+++ canvas/dibujo.js
@@ -0,0 +1,21 @@
+var d = document.getElementById("dibujito");
+var lienzo = d.getContext("2d");
+
+dibujarLinea("pink",10,390,10,10);
+dibujarLinea("pink",10,10,390,10);
+dibujarLinea("pink",10,390,390,390);
+dibujarLinea("pink",390,390,390,10);
+
+dibujarLinea("green ",210,10,10,200);
+dibujarLinea("blue",10,200,390,200);
+dibujarLinea("yellow",390,200,210,10);
+
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
+{
+    lienzo.beginPath();
+    lienzo.strokeStyle = color;
+    lienzo.moveTo(xinicial,yinicial);
+    lienzo.lineTo(xfinal,yfinal);
+    lienzo.stroke();
+    lienzo.closePath();
+}
